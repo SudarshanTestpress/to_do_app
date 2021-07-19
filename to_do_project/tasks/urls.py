384ada36b93bf430149cfd5bde13fdb904d1e955
Iphone6sisgood <sudarshan@testpress.in>
2021-07-19 18:58:35 +0530

    created project update view

--- to_do_project/tasks/urls.py
+++ to_do_project/tasks/urls.py
@@ -7,6 +7,7 @@ app_name = 'tasks'
 
 urlpatterns = [
     path('projects/',views.ProjectListView.as_view(), name = 'list_project'),
-    path('projects/<str:pk>',views.TaskListView.as_view(), name= 'list_task')
+    path('projects/<str:pk>',views.TaskListView.as_view(), name= 'list_task'),
+    path('projects/<str:pk>/updatename',views.ProjectUpdateView.as_view(), name = 'update_project')
     
     ]
