c633eac09c5f364ed09f979ffb603a1ec7202dfe
Iphone6sisgood <sudarshan@testpress.in>
2021-07-19 18:34:57 +0530

    created Task List  view

--- to_do_project/tasks/tests.py
+++ to_do_project/tasks/tests.py
@@ -1,13 +1,16 @@
 import random
+from .views import ProjectListView, TaskListView
 from django.test import TestCase
 from django.http import response
 from django.urls import reverse, resolve
-from tasks.models import Task, Project
+from .models import Task, Project
 from .forms import ProjectForm
+
 
 # Create your tests here.
 class FormTest(TestCase):
     def setUp(self):
+        self.url = reverse('list_view')
         self.response = self.client.get(self.url)
 
     def test_projectform_inputs(self):
@@ -77,4 +80,66 @@ class ModelTest(TestCase):
 
 
 
+class TestProjectListView(TestCase):   
+    def setUp(self):
+            self.project1 = Project.objects.create(
+            name = 'Deployment')
+
+        
+
+            self.task1 = Task.objects.create(
+                text = 'Eat',
+                project = self.project1,
+                completed = True
+            )
+
+            self.task2 = Task.objects.create(
+                text = 'Sleep',
+                project = self.project1,
+                completed = False
+            )
+
+            self.url = reverse('tasks:list_project')
+            self.response = self.client.get(self.url)
+
+
+    def test_page_serve_successful(self):
+        self.assertEquals(self.response.status_code, 200)
+
+
+    def test_project_list_object_is_served(self):
+        view = resolve('/projects/')
+        self.assertEquals(view.func.view_class, ProjectListView)
+
+
+class TestTaskListView(TestCase):
+    def setUp(self):
+            self.project1 = Project.objects.create(
+            name = 'Deployment')
+
+        
+
+            self.task1 = Task.objects.create(
+                text = 'Eat',
+                project = self.project1,
+                completed = True
+            )
+
+            self.task2 = Task.objects.create(
+                text = 'Sleep',
+                project = self.project1,
+                completed = False
+            )
+
+            self.url = reverse('tasks:list_project')
+            self.response = self.client.get(self.url)
+            self.url = reverse('tasks:list_task',args=[self.project1.pk])
+            self.response = self.client.get(self.url)
+    
+    def test_page_serve_successful(self):
+        self.assertEquals(self.response.status_code, 200)
+    
+    def test_project_list_object_is_served(self):
+        view = resolve('/projects/1')
+        self.assertEquals(view.func.view_class, TaskListView)
 
--- to_do_project/tasks/urls.py
+++ to_do_project/tasks/urls.py
@@ -6,6 +6,7 @@ from . import views
 app_name = 'tasks'
 
 urlpatterns = [
-    path('projects/',views.ProjectList.as_view(), name = 'create_and_list_project'),
+    path('projects/',views.ProjectListView.as_view(), name = 'list_project'),
+    path('projects/<str:pk>',views.TaskListView.as_view(), name= 'list_task')
     
     ]
