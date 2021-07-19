2f3f5a154d845c4d2a344763492b6ca08305d2cb
Iphone6sisgood <sudarshan@testpress.in>
2021-07-19 19:30:38 +0530

    created project delete view

--- to_do_project/tasks/urls.py
+++ to_do_project/tasks/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.urls.resolvers import URLPattern
 from . import views
 
 
@@ -8,6 +7,7 @@ app_name = 'tasks'
 urlpatterns = [
     path('projects/',views.ProjectListView.as_view(), name = 'list_project'),
     path('projects/<str:pk>',views.TaskListView.as_view(), name= 'list_task'),
-    path('projects/<str:pk>/updatename',views.ProjectUpdateView.as_view(), name = 'update_project')
+    path('projects/<str:pk>/updatename',views.ProjectUpdateView.as_view(), name = 'update_project'),
+    path('projects/<str:pk>/deletename', views.ProjectDeleteView.as_view(), name = 'delete_project')
     
     ]
