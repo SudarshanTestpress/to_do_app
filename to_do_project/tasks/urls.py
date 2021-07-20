2342152b4fa54e6c7034f4d8ce96764eb2e17cc1
Iphone6sisgood <sudarshan@testpress.in>
2021-07-20 17:36:54 +0530

    created task delete view

--- to_do_project/tasks/urls.py
+++ to_do_project/tasks/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('projects/<str:pk>/delete', views.ProjectDeleteView.as_view(), name = 'delete_project'),
     path('projects/<str:pk>/tasks',views.TaskListView.as_view(), name= 'list_task'),
     path('projects/<str:pk>/tasks/create',views.TaskCreateView.as_view(), name='create_task'),
-    path('projects/<str:pk>/tasks/<str:task_pk>/update',views.TaskUpdateView.as_view(), name = 'update_task')
+    path('projects/<str:pk>/tasks/<str:task_pk>/update',views.TaskUpdateView.as_view(), name = 'update_task'),
+    path('projects/<str:pk>/tasks/<str:task_pk>/delete',views.TaskDeleteView.as_view(), name = 'delete_task')
     
     ]
